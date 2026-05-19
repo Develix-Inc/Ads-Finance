612a9b7b090d0d29b92fd175be41a7591ba1f0de
XAGASKII101 <alexiusdubem7@gmail.com>
2026-05-19 14:03:59 +0100

    chore: parameterize firebase config with env variables, setup .env.local

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATf0t63qcDyMUT895MzFCpt3PMLe_Q8hE",
-  authDomain: "adsfinancearn.firebaseapp.com",
-  projectId: "adsfinancearn",
-  storageBucket: "adsfinancearn.firebasestorage.app",
-  messagingSenderId: "392037356679",
-  appId: "1:392037356679:web:07f40335ed7e7cf7f95422"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyATf0t63qcDyMUT895MzFCpt3PMLe_Q8hE",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "adsfinancearn.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "adsfinancearn",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "adsfinancearn.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "392037356679",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:392037356679:web:07f40335ed7e7cf7f95422"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
